f1b5ed417278da6a12f658916fd82c003e3a6386
Nick Bolles <me@nickbolles.com>
2020-03-01 20:31:44 -0600

    export options interfaces

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,4 @@ export * from './typegoose.decorators';
 export * from './typegoose.module';
 export * from './typegoose.utils';
 export * from './typegoose-options.interface';
+export * from './typegoose-class.interface.ts';
